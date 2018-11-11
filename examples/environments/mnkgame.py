51b1ae72ce7d5860ae0d490d46be2221095daebe
Jontahan <jona.jorg@gmail.com>
2018-11-11 15:19:06 +0100

    Shorten M,N,K win check function

--- examples/environments/mnkgame.py
+++ examples/environments/mnkgame.py
@@ -1,13 +1,14 @@
 import numpy as np
+import operator as op
 
 class MNKGame:
-    def __init__(self, M, N, K):
+    def __init__(self, M, N, K, starting_piece=1):
         self.M = M # rows
         self.N = N # columns
         self.K = K
         self.state = np.zeros((M, N)) # [row][column] / [y][x]
         self.action_space = range(M * N)
-        self.player_piece = 1
+        self.player_piece = starting_piece
 
     def is_legal_action(self, state, action):
         return state[action // self.N][action % self.N] == 0
@@ -16,87 +17,86 @@ class MNKGame:
         done = self.is_winning_move(self.state, action)
         self.state[action // self.N][action % self.N] = self.player_piece
         self.player_piece *= -1
-        return done
+        return self.state, done
+    
+    def simulate(self, state, action):
+        done = self.is_winning_move(state, action)
+        state[action // self.N][action % self.N] = self.player_piece
+        self.player_piece *= -1
+        return self.state, done
 
     # run this before flipping the player piece
     def is_winning_move(self, state, action):
         y, x = action // self.N, action % self.N
-        # horizontal
-        length = 1
-
-        offset = 1
-        while x - offset >= 0 and self.state[y][x - offset] == self.player_piece:
-            offset += 1
-            length += 1
-
-        offset = 1
-        while x + offset < self.N and self.state[y][x + offset] == self.player_piece:
-            offset += 1
-            length += 1
+        x_range, y_range = range(0, self.N), range(0, self.M)
         
-        if length >= self.K:
-            return True
-
-        # vertical
-        length = 1
-
-        offset = 1
-        while y - offset >= 0 and self.state[y - offset][x] == self.player_piece:
-            offset += 1
-            length += 1
-
-        offset = 1
-        while y + offset < self.M and self.state[y + offset][x] == self.player_piece:
-            offset += 1
-            length += 1
+        idf = lambda x, y : x # identity function
         
-        if length >= self.K:
-            return True
-
-        # diagonal descending
+        ops = [[op.sub, idf],    # horizontal
+               [op.add, idf],    # horizontal
+               [idf, op.sub],    # vertical
+               [idf, op.add],    # vertical
+               [op.sub, op.sub], # diagonal desc
+               [op.add, op.add], # diagonal desc
+               [op.sub, op.add], # diagonal asc
+               [op.add, op.sub]] # diagonal asc
+               
         length = 1
-
-        offset = 1
-        while x - offset >= 0 and y - offset >= 0 and self.state[y - offset][x - offset] == self.player_piece:
-            offset += 1
-            length += 1
-
-        offset = 1
-        while x + offset < self.N and y + offset < self.M and self.state[y + offset][x + offset] == self.player_piece:
-            offset += 1
-            length += 1
-        
-        if length >= self.K:
-            return True
-
-        # diagonal ascending
-        length = 1
-
-        offset = 1
-        while x - offset >= 0 and y + offset < self.M and self.state[y + offset][x - offset] == self.player_piece:
-            offset += 1
-            length += 1
-
-        offset = 1
-        while x + offset < self.N and y - offset >= 0 and self.state[y - offset][x + offset] == self.player_piece:
-            offset += 1
-            length += 1
-        
-        if length >= self.K:
-            return True
+        for i in range(len(ops)):
+            op_list = ops[i]
             
+            offset = 1
+            while op_list[0](x, offset) in x_range and \
+                  op_list[1](y, offset) in y_range and \
+                  self.state[op_list[1](y, offset)][op_list[0](x, offset)] == self.player_piece:
+                offset += 1
+                length += 1
+            
+            if i % 2 == 1:
+                if length >= self.K:
+                    return True    
+                length = 1
+        
         return False
+        
+    def reset(self, starting_piece):
+        self.state = np.zeros((self.M, self.N))
+        self.player_piece = starting_piece
 
 ttt = MNKGame(4, 3, 3)
 
-print(ttt.state)
-print(ttt.step(2))
-print(ttt.step(7))
-print(ttt.step(4))
-print(ttt.step(5))
-print(ttt.step(6))
-print(ttt.step(9))
-print(ttt.state)
+ttt.state = np.array([[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 0],
+                      [0, 0, 0]])
+ttt.player_piece = 1
+assert ttt.step(8)[1] == True
 
-#for action in ttt.action_space:
-#    ttt.step(action)
+ttt.state = np.array([[1, 0, 0],
+                      [0, 1, 0],
+                      [1, 0, 0],
+                      [0, 0, 0]])
+ttt.player_piece = 1
+assert ttt.step(2)[1] == True
+
+ttt.state = np.array([[0, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 0],
+                      [0, 1, 0]])
+ttt.player_piece = 1
+assert ttt.step(1)[1] == False
+
+ttt.state = np.array([[1, 0, 0],
+                      [0, 1, 0],
+                      [0, 0, 0],
+                      [1, 1, 0]])
+ttt.player_piece = 1
+assert ttt.step(11)[1] == True
+
+ttt.state = np.array([[1, 0, 0],
+                      [0, 0, 1],
+                      [0, 0, 1],
+                      [0, 0, 0]])
+ttt.player_piece = 1
+
+assert ttt.step(2)[1] == True

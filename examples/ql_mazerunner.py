b742c047227729f52d16575191869169dd3e9e4f
Jontahan <jona.jorg@gmail.com>
2019-02-14 10:31:28 +0100

    Cleanup in mazerunner, still considering whether or not to use agent class here

--- examples/ql_mazerunner.py
+++ examples/ql_mazerunner.py
@@ -1,21 +1,22 @@
 import pygame as pg
-from environments.maze import MazeEnvironment
-from vicero.algorithms.qlearning import Qlearning
 import numpy as np
 import matplotlib.pyplot as plt
+from environments.maze import MazeEnvironment
+from vicero.algorithms.qlearning import Qlearning
+from vicero.agent import Agent
 
-np.random.seed()
+board = [[0  ,0  ,0  ,0  ,10 ,0  ,0  ,0  ],
+         [0  ,0  ,-1 ,-1 ,-1 ,0  ,0  ,0  ],
+         [0  ,0  ,-1 ,0  ,0  ,-1 ,0  ,0  ],
+         [0  ,0  ,0  ,0  ,0  ,0  ,0  ,-1 ],
+         [10 ,0  ,0  ,0  ,0  ,0  ,0  ,0  ],
+         [0  ,0  ,0  ,0  ,0  ,0  ,0  ,0  ],
+         [0  ,0  ,0  ,-1 ,-1 ,0  ,0  ,0  ],
+         [0  ,0  ,0  ,1  ,0  ,0  ,0  ,0  ]]
 
-board = [[0 ,0 ,0 ,0 ,10,0 ,0 ,0 ],
-         [0 ,0 ,-1,-1,-1,0 ,0 ,0 ],
-         [0 ,0 ,-1,0 ,0 ,-1,0 ,0 ],
-         [0 ,0 ,0 ,0 ,0 ,0 ,0 ,-1],
-         [10,0 ,0 ,0 ,0 ,0 ,0 ,0 ],
-         [0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ],
-         [0 ,0 ,0 ,-1,-1,0 ,0 ,0 ],
-         [0 ,0 ,0 ,1 ,0 ,0 ,0 ,0 ]]
+board = np.array(board)
 
-cell_size  = 32 # the size of one game cell, in pixels
+cell_size  = 48 # the size of one game cell, in pixels
 pad_cells  = 1  # padding between the visualizations
 framerate  = 60 # frames per second
 
@@ -25,54 +26,27 @@ screen = pg.display.set_mode((cell_size * len(board[0]), cell_size * len(board))
 clock = pg.time.Clock()
 
 class GameInstance:
-    def __init__(self, env, model, offset):
+    def __init__(self, env, algorithm):
         self.env = env
-        self.model = model
-        self.offset = offset
-        # steps_taken is the number of steps taken before a completion of the maze
-        # Iteration is the number of times the agent has completed the environment
-        self.info = {'pos': (3, 7), 'steps_taken': 0}
+        self.algorithm = algorithm
+        self.steps_taken = 0
         self.step_history = []
 
-    def get_epsilon(self):
-        return self.model.get_epsilon()
-
     def game_step(self):
-        # discretize current game state
-        self.info['steps_taken'] += 1
+        self.steps_taken += 1
         
         state = self.env.state
-        
-        # let the model choose an action
-        action = self.model.exploratory_action(state)
-
-        # run one step in the simulation
+        action = self.algorithm.exploratory_action(state)
         new_state, reward, done, self.board = self.env.step(action)
+        self.algorithm.update_q(state, action, reward, new_state)
         
-        # update the Q table based on the observation
-        self.model.update_q(state, action, reward, new_state)
-        
-        # visualize the new state
-        self.draw_world()
-        
-        # if in goal state, restart
         if done:
             self.env.reset()
-            self.step_history.append(self.info['steps_taken'])
-            self.info['steps_taken'] = 0
-            info = {'x' : 3, 'y' : 7}
-
+            self.step_history.append(self.steps_taken)
+            self.steps_taken = 0
+        
             return self.step_history,  True
         return None,  False
-
-    def draw_world(self):
-        heatmap = np.ndarray(self.board.shape)
-        for i in range(len(self.board[0])):
-            for j in range(len(self.board)):
-                qval = np.average(self.model.Q[discretize((i, j))])
-                heatmap[i][j] = 64 + 50 * qval
-                
-        self.env.draw(screen, heatmap)
 
 env = MazeEnvironment(board, cell_size)
 
@@ -80,8 +54,7 @@ def discretize(state):
     return state[1] * env.size + state[0]
 
 ql = Qlearning(env, len(board) ** 2, len(MazeEnvironment.action_space), epsilon=0.1, discretize=discretize)
-
-game = GameInstance(env, ql, (0, 0))
+game = GameInstance(env, ql)
 
 def plot_durations(steps):
     plt.figure(1)
@@ -95,8 +68,17 @@ def plot_durations(steps):
 
 while True:
     i = 0
-    step_list, finished = game.game_step()
-    if finished:
+    step_list, done = game.game_step()
+    if done:
         plot_durations(step_list)
+    
+    heatmap = np.ndarray(board.shape)
+    for i in range(len(board[0])):
+        for j in range(len(board)):
+            qval = np.average(ql.Q[discretize((i, j))])
+            heatmap[i][j] = 64 + 50 * qval
+
+    env.draw(screen, heatmap)
+
     pg.display.flip()
     clock.tick(framerate)

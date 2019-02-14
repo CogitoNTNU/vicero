70fea8e9d6693feba3f6f01f25c184bb02f85d35
Jontahan <jona.jorg@gmail.com>
2019-02-14 08:40:20 +0100

    Minor cleanup in the policy example

--- examples/ql_maze_policy.py
+++ examples/ql_maze_policy.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import environments.maze as maze
+from environments.maze import MazeEnvironment
 from vicero.algorithms.qlearning import Qlearning
 from vicero.policy import RandomPolicy
 import numpy as np
@@ -17,6 +17,8 @@ board = [[0 ,0 ,0 ,0 ,10,0 ,0 ,0 ],
          [0 ,0 ,0 ,0 ,0 ,0 ,0 ,0 ],
          [0 ,0 ,0 ,-1,-1,0 ,0 ,0 ],
          [0 ,0 ,0 ,1 ,0 ,0 ,0 ,0 ]]
+
+# TODO: move visualization etc. to the MazeEnvironment class..
 
 cell_size  = 48 # the size of one game cell, in pixels
 framerate  = 10 # frames per second
@@ -50,33 +52,38 @@ class GameInstance:
                 if self.board[j][i] == 10:
                     pg.draw.rect(screen, (180, 180, 64), pg.Rect(cell_size * i, cell_size * j, cell_size, cell_size))
 
-env = maze.MazeEnvironment(board)
+env = MazeEnvironment(board)
 
-ql = Qlearning(env, len(board) ** 2, len(maze.MazeEnvironment.action_space), epsilon=0.15, discretize=lambda state : state[1] * env.size + state[0])
+ql = Qlearning(env, len(board) ** 2, len(MazeEnvironment.action_space), epsilon=0.15, discretize=lambda state : state[1] * env.size + state[0])
 
+# Random for benchmarking
+random_policy = RandomPolicy(MazeEnvironment.action_space)
+
+# Train very little
 ql.train(100)
-better_policy = ql.copy_target_policy()
+policy_one = ql.copy_target_policy()
 
+# Train some more
 ql.train(3000)
-decent_policy = ql.copy_target_policy()
+policy_two = ql.copy_target_policy()
 
-ql.train(10000)
-pro_policy = ql.copy_target_policy()
+# Train way more
+ql.train(20000)
+policy_three = ql.copy_target_policy()
 
-game = GameInstance(env, better_policy)
+game = GameInstance(env, random_policy)
 
-random_policy = RandomPolicy(maze.MazeEnvironment.action_space)
-
-policies = [('the random policy', random_policy),
-            ('the bad policy', better_policy),
-            ('the not good enough policy', decent_policy),
-            ('the good policy', pro_policy)]
+policies = [('the random policy',          random_policy),
+            ('the bad policy',             policy_one),
+            ('the not good enough policy', policy_two),
+            ('the good policy',            policy_three)]
 
 while True:
     for policy in policies:
         print('switching to {}'.format(policy[0]))
         
         game.policy = policy[1]
+
         for _ in range(30):
             game.game_step()
             pg.display.flip()

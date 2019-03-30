c3445f3074467e343644d33ef07fd76d6d87c485
Jontahan <jona.jorg@gmail.com>
2019-03-30 16:28:21 +0100

    ActionDistributionOverlay added

---
+++ vicero/visualization/overlay.py
@@ -0,0 +1,20 @@
+import pygame as pg
+
+class Overlay:
+    def __init__(self, rect):
+        self.rect = rect
+
+class ActionDistributionOverlay(Overlay):
+    def __init__(self, algorithm, rect):
+        super(ActionDistributionOverlay, self).__init__(rect)
+        self.algorithm = algorithm
+
+    def render(self, screen, state):
+        sm = self.algorithm.action_distribution(state)
+        bar_w = self.rect.w // len(sm)
+        
+        for i in range(len(sm)):
+            pg.draw.rect(screen, (0, 200, 0), pg.Rect(self.rect.x + i * bar_w, self.rect.h, bar_w, -self.rect.h * sm[i]))
+            pg.draw.rect(screen, (0, 160, 0), pg.Rect(self.rect.x + i * bar_w, self.rect.h, bar_w, -self.rect.h * sm[i]), 1)
+        
+

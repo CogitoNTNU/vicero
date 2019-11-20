1692e585ceec549121c1fbb6eb1a7c381e8b7565
Jontahan <jona.jorg@gmail.com>
2019-11-20 13:24:27 +0100

    Add LunarLander-v2 example using REINFORCE

--- examples/pg_lunarlander.py
+++ examples/pg_lunarlander.py
@@ -21,20 +21,21 @@ def plot(history):
             plt.plot(means.numpy())
 
         plt.pause(0.001)
+    
 
 class PolicyNet(nn.Module):
     def __init__(self):
         super(PolicyNet, self).__init__()
-        self.fc1 = nn.Linear(4, 24)
+        self.fc1 = nn.Linear(8, 24)
         self.fc2 = nn.Linear(24, 36)
-        self.fc3 = nn.Linear(36, 1)
+        self.fc3 = nn.Linear(36, 4)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
+        x = F.softmax(self.fc3(x), dim=-1)
         return x
 
-env = gym.make('CartPole-v0')
-pg = Reinforce(env, polinet=PolicyNet(), learning_rate=0.01, gamma=0.99, batch_size=5, plotter=plot)
-pg.train(1000)
+env = gym.make('LunarLander-v2')
+pg = Reinforce(env, polinet=PolicyNet(), learning_rate=0.01, gamma=0.99, batch_size=8, plotter=plot)
+pg.train(10000)

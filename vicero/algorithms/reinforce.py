559f0f827740995f4065da5f584fdaee37fcaec7
Jontahan <jona.jorg@gmail.com>
2019-11-20 13:23:40 +0100

    Update REINFORCE to handle all discrete action spaces

--- vicero/algorithms/reinforce.py
+++ vicero/algorithms/reinforce.py
@@ -1,25 +1,12 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.distributions import Bernoulli
+from torch.distributions import Bernoulli, Categorical
 from itertools import count
 import numpy as np
 
-class PolicyNet(nn.Module):
-    def __init__(self):
-        super(PolicyNet, self).__init__()
-        self.fc1 = nn.Linear(4, 24)
-        self.fc2 = nn.Linear(24, 36)
-        self.fc3 = nn.Linear(36, 1)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = torch.sigmoid(self.fc3(x))
-        return x
-
 class Reinforce:
-    def __init__(self, env, polinet=PolicyNet(), learning_rate=0.01, gamma=0.99, batch_size=1, plotter=None):
+    def __init__(self, env, polinet, learning_rate=0.01, gamma=0.99, batch_size=5, plotter=None):
         self.policy_net = polinet
         self.optimizer = torch.optim.RMSprop(self.policy_net.parameters(), lr=learning_rate)
         self.episode_history = []
@@ -27,6 +14,7 @@ class Reinforce:
         self.batch_size = batch_size
         self.env = env
         self.plotter = plotter
+        self.binary_action_space = (list(polinet.modules())[-1].out_features == 1)
 
     def gradient_step(self, trajectories):
         
@@ -63,31 +51,39 @@ class Reinforce:
                 state, action, reward = trajectory[j]
 
                 probs = self.policy_net(state)
-                m = Bernoulli(probs)
+                
+                m = Bernoulli(probs) if self.binary_action_space else Categorical(probs)
 
                 loss = -m.log_prob(action) * reward
+                
                 loss.backward()
             
         self.optimizer.step()
 
     def train(self, n_episodes):
+        render = False
         trajectories = []
         for e in range(n_episodes):
+            
             trajectory = []
             state = torch.from_numpy(self.env.reset()).float()
-            #if render: env.render(mode='rgb_array')
+            if render: self.env.render(mode='rgb_array')
 
             for t in count():
                 probs = self.policy_net(state)
                 
-                m = Bernoulli(probs)
-
-                action = m.sample().data.numpy().astype(int)[0]
+                if self.binary_action_space:
+                    m = Bernoulli(probs)
+                    action = m.sample().data.numpy().astype(int)[0]
+                else: 
+                    m = Categorical(probs)
+                    action = m.sample()
                 
-                next_state, reward, done, _ = self.env.step(action)
+                next_state, reward, done, _ = self.env.step(action.item())
                 
-                #if render: env.render(mode='rgb_array')
-                #if done: reward = 0
+                if render: self.env.render(mode='rgb_array')
+                
+                # TODO: add log prob to trajectory, optimization
 
                 trajectory.append((state, action, reward))            
                 state = torch.from_numpy(next_state).float()
